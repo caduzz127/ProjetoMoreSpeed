ffe80ce452f863a61500d1f32c291e641c653047
Carlos Eduardo <melocaduzinho@gmail.com>
2026-04-07 16:56:34 -0300

    atualizando as mensagens da

--- assets/js/mensagens.js
+++ assets/js/mensagens.js
@@ -1,5 +1,6 @@
 const telefoneSuporte = "5521965655662";
-
+const telefoneInstalacao = "5521965615909";
+const telefoneSuporteCorporativo = "5521985908839";
 
 
 function enviarMensagemWhatsApp() {
@@ -14,16 +15,16 @@ function enviarMensagemWhatsApp() {
         return;
     }
 
-    // Montar mensagem formatada para WhatsApp
+    
     const textoMensagem = `Olá, meu nome é ${nome}. Meu e-mail é ${email}. Meu telefone é ${telefone}. Mensagem: ${mensagem}`;
 
-    // Abrir WhatsApp com a mensagem
+   
     const url = `https://wa.me/${telefoneSuporte}?text=${encodeURIComponent(textoMensagem)}`;
     window.open(url, '_blank');
 }
 
 
-const telefoneInstalacao = "5521965615909";
+
 const plano200 = "Eu gostaria de Saber mais sobre o Plano de 200 Megas. Estou vindo pelo site";
 const plano300 = "Eu gostaria de Saber mais sobre o Plano de 300 Megas. Estou vindo pelo site";
 const plano450 = "Eu gostaria de Saber mais sobre o Plano de 450 Megas. Estou vindo pelo site";
@@ -54,4 +55,16 @@ function paginaCliente() {
     window.location.href = "http://atendimento.morespeed.com.br:8000/accounts/central/login";
 }
 
-
+function enviarMensagemWhatsAppCorporativo() {
+    const nomeDaEmpresa = document.getElementById('nomeC').value;
+    const email = document.getElementById('emailC').value;
+    const telefone = document.getElementById('telefoneC').value;
+    const mensagem = document.getElementById('mensagemC').value;
+    if (!nomeDaEmpresa || !email || !telefone || !mensagem) {
+        alert('Por favor, preencha todos os campos!');
+        return;
+    }
+    const mensagemCorporativo = `Olá, gostaria de falar com um especialista sobre os planos corporativos. Estou vindo pelo site.\nNome da Empresa: ${nomeDaEmpresa}\nE-mail: ${email}\nTelefone: ${telefone}\nMensagem: ${mensagem}`;
+    const url = `https://wa.me/${telefoneSuporteCorporativo}?text=${encodeURIComponent(mensagemCorporativo)}`;
+    window.open(url, '_blank');
+}
