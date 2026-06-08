aa269a470dd28d48a7b4804ccf8836be033058dd
Carlos Eduardo <melocaduzinho@gmail.com>
2026-06-08 19:52:17 -0300

    Atualizar o modalPalpiteCopa.js
    Atualização na aparição do modal da copa do mundo

--- assets/js/modalPalpiteCopa.js
+++ assets/js/modalPalpiteCopa.js
@@ -2,15 +2,14 @@
 console.log("Script modalPalpiteCopa.js carregado com sucesso!");
 
 window.addEventListener("load", function () {
-    if (!localStorage.getItem("modalCopaVisto")) {
+
         document.getElementById("modalCopa").style.display = "flex";
-    }
+    
 });
 
 function fecharModalCopa() {
     const iconeBolaDeFutebol = document.getElementById("bola-flutuante");
     iconeBolaDeFutebol.classList.remove("inativo");
     document.getElementById("modalCopa").style.display = "none";
-    localStorage.setItem("modalCopaVisto", "true");
-    localStorage.removeItem("modalCopaVisto");
+    
 }
