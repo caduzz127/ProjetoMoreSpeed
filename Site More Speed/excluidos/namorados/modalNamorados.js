8416a3dc71b18fe54648b8fa7e955b8aad1dba5d
Carlos Eduardo <melocaduzinho@gmail.com>
2026-09-10 11:24:48 -0300

    Revert "Refatorando estrutura de pastas do projeto"
    This reverts commit b7fbdedbe04df78071fad9a8d266a0f158eaa7c5.

--- Site More Speed/excluidos/namorados/modalNamorados.js
+++
@@ -1,8 +0,0 @@
-console.log("Script modalNamorados.js carregado com sucesso!");
-window.addEventListener("load", function() {
-    console.log("modal dos namorados: ", document.getElementById("modalNamorados"));
-    document.getElementById("modalNamorados").style.display = "flex";
-});
-function fecharModalNamorados(){
-    document.getElementById("modalNamorados").style.display = "none";
-}
